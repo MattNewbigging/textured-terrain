6d557e1a0702dbe15b09d1f15abc9a44241b8247
MattNewbigging <matt.newbigging@aize.io>
2025-07-18 13:04:01 +0100

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-tester/", // replace with the repo name
+  base: "/textured-terrain/", // replace with the repo name
   assetsInclude: ["**/*.gltf"],
   publicDir: "public",
   build: {
